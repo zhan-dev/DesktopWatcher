da865bcd1177e64b6b0c84ec1a1e71f7a3d2710d
Zhan <Zhan.Kariahin@gmail.com>
2025-04-30 00:13:44 +0300

    Add project files.

---
+++ DesktopWatcher/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace DesktopWatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.history_lbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // history_lbl
+            // 
+            this.history_lbl.AutoSize = true;
+            this.history_lbl.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.history_lbl.Location = new System.Drawing.Point(13, 9);
+            this.history_lbl.Name = "history_lbl";
+            this.history_lbl.Size = new System.Drawing.Size(72, 21);
+            this.history_lbl.TabIndex = 0;
+            this.history_lbl.Text = "History: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(695, 374);
+            this.Controls.Add(this.history_lbl);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label history_lbl;
+    }
+}
+
---
+++ DesktopWatcher/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace DesktopWatcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            history_lbl.Text = $"Desktop history: {Environment.NewLine}";
+            FileSystemWatcher watcher = new FileSystemWatcher(Environment.GetFolderPath(Environment.SpecialFolder.Desktop))
+            { EnableRaisingEvents = true, SynchronizingObject = this };
+
+            watcher.Created += Watcher_Created;
+            watcher.Renamed += Watcher_Renamed;
+            watcher.Changed += Watcher_Changed;
+            watcher.Deleted += Watcher_Deleted;
+        }
+
+        private void Watcher_Created(object sender, FileSystemEventArgs e) => history_lbl.Text += 
+            $@"Юзер {Environment.UserDomainName} ({Environment.UserName}) создал '{e.Name}' | {DateTime.Now}{Environment.NewLine}";
+
+        private void Watcher_Renamed(object sender, RenamedEventArgs e) => history_lbl.Text +=
+            $"Юзер {Environment.UserDomainName} ({Environment.UserName}) переименовал '{e.OldName}' в '{e.Name}' | {DateTime.Now}{Environment.NewLine}";
+
+        private void Watcher_Changed(object sender, FileSystemEventArgs e) => history_lbl.Text +=
+            $"Юзер {Environment.UserDomainName} ({Environment.UserName}) изменил '{e.Name}' | {DateTime.Now}{Environment.NewLine}";
+
+        private void Watcher_Deleted(object sender, FileSystemEventArgs e) => history_lbl.Text +=
+            $"Юзер {Environment.UserDomainName} ({Environment.UserName}) удалил '{e.Name}' | {DateTime.Now}{Environment.NewLine}";
+    }
+}
